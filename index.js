9ee26e06be4fed2627fc93a5a48e7f82e196102d
Ashley King <ashleyot@gmail.com>
2019-05-09 12:04:35 -0400

    added some middleware

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 // for env vars
 require('dotenv').config()
 
-//initialize express
+//dependencies
+const debug = require('debug')('app:startup');
+const config = require('config');
 const express = require('express');
 const app = express();
-// const bodyParser = require('body-parser')
+const logger = require('./logger')
+const authenticator = require('./authentication')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const pug = require('pug');
 
-app.use(express.json());
+
+//configuration
+console.log("Application Name " + config.get('name'))
+console.log("Mail Server Name " + config.get('mail.host'))
+
+
+
+//middleware
+app.use(express.json()); //parses req.body
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(logger);
+app.use(authenticator);
+app.use(helmet());
+
+// enable console logging of requests in dev
+if(app.get('env') === 'development'){
+  app.use(morgan('tiny'));
+  debug("Morgan enabled - dev environment")
+}
+
+//db debugger
+debug("Connected to database...");
+
 //configure port for main server
 const port = process.env.PORT || 3000;
 
